3c1b4b03b304b9ec43af320fc6edf6c0122e77a7
OppedRawen <yud2372@gmail.com>
2023-02-02 20:46:26 -0800

    boiler plate code

---
+++ script.js
@@ -0,0 +1,5 @@
+var input = $("search-input");
+
+function getApi(){
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=b3bc862dbbd015f43fc00d688d65a36c";
+}
