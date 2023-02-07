428bfa998c77f6a55e20624399120f798bd57540
nshaw973 <nshaw973@gmail.com>
2023-02-06 21:08:04 -0800

    added youtube video search function based on movie title searched for.

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ function getApi(userInput){
                     title.textContent = input.value;
                     var awards = document.querySelector("#awards");
                     awards.textContent= data.Awards;
+                    var movieTitle = data.Title
+                  console.log(movieTitle)
                     var plot = document.querySelector("#plot");
                     var actor = document.querySelector('#actor');
                     var rated = document.querySelector("#rated");
@@ -33,9 +35,28 @@ function getApi(userInput){
                     plot.textContent = data.Plot;
                     actor.textContent = data.Actors;
                     rated.textContent = data.Rated
+                    getYoutubeVideo(movieTitle);
+                    
                 }})
             }
         })
 
     
 }
+
+//This searches for a youtube video based on the movie title given in the previous function
+//added full original sountrack to search to look specifically for that movies soundtrack.
+function getYoutubeVideo(movieTitle) {
+    var requestUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + movieTitle + "full original soundtrack&type=video&key=AIzaSyCJTHFOR8cX7fWfJ_0L1mLrsfgvneAZnsk";
+    console.log(requestUrl)
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data.items[1].id.videoId)
+        var videoID = data.items[1].id.videoId
+        var youtubeVideo = "https://www.youtube.com/watch?v=" + videoID
+        console.log(youtubeVideo)       
+      })
+  }
